61ada346c111b4edb382a1ac79bf72602b8a2627
partimecoderlin <insidetom@163.com>
2022-05-25 16:16:40 +0800

    在base app中创建home和room两个页面
    使用dummy room data

--- base/views.py
+++ base/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+dummy_room_data = [
+    {
+        'id': 1,
+        'name': 'python web dev'
+    },
+    {
+        'id': 2,
+        'name': 'nodejs web dev'
+    },
+    {
+        'id': 3,
+        'name': 'javascripts web dev'
+    },
+]
+
+
+def home(request):
+    context = {'rooms': dummy_room_data}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = None
+    for i in dummy_room_data:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
