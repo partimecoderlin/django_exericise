9d1863323cb644ddc64e58e8ed9e9b273eb56dac
xuefei.lin <insidetom@hotmail.com>
2022-05-27 10:53:20 +0800

    添加页面数据操作功能
    新建room,修改room

--- base/views.py
+++ base/views.py
@@ -30,12 +30,19 @@ def createRoom(request):
     return render(request, 'base/room_form.html', context)
 
 def updateRoom(request,pk):
+    # 在room表中取得id为pk的数据
     room=models.Room.objects.get(id=int(pk))
+    # 用上面的数据生成form供用户输入
     form=RoomForm(instance=room)
+    # 当客户端post数据到这个view
     if request.method=='POST':
+        # 用room查找到数据表中的数据，并用post数据更新数据表
         form=RoomForm(request.POST,instance=room)
+        # 如果form的数据合法？
         if form.is_valid():
+            # 保存数据和重新引导用户会主页
             form.save()
             return redirect('home')
+    # 供用户输入的form作为参数传给render
     context={'form':form}
     return render(request,'base/room_form.html',context)
